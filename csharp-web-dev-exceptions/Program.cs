e64ed9ca06b11129c524f220d0e32756cbbd468c
Annalyssa Stakes <annalyssastakes@Annalyssas-Air.attlocal.net>
2022-09-27 15:50:32 -0500

    exceptions exercises complete

--- csharp-web-dev-exceptions/Program.cs
+++ csharp-web-dev-exceptions/Program.cs
@@ -7,21 +7,54 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
-        /*
+
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            if (y == 0)
+            {
+                throw new ArgumentOutOfRangeException("y", "Value of Y cannot be 0.");
+            }
+            else
+            {
+                return x / y;
+            }
         }
+
+
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException("fileName", "No work submitted.");
+            }
+            else 
+            {
+                if (fileName.Substring(fileName.Length - 3, 3) == ".cs")
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
         }
-        */
+
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+
+            try
+            {
+                Divide(10, 0);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -30,7 +63,10 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
 
-
+            Console.WriteLine(CheckFileExtension(students["Carl"]));
+            Console.WriteLine(CheckFileExtension(students["Brad"]));
+            Console.WriteLine(CheckFileExtension(students["Elizabeth"]));
+            Console.WriteLine(CheckFileExtension(students["Stefanie"]));
         }
     }
 }
